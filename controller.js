90836cbc397476b00bf37656029c0cce2964ace1
Linda Tran <133718039+lindatranlt@users.noreply.github.com>
2024-01-28 18:46:03 -0800

    initializing assessment 4

---
+++ controller.js
@@ -0,0 +1,37 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+
+
+    getFortune: (req, res) => {
+        const fortunes = ["A career in Cybersecurity awaits you!", "You will pass your assessment!", "You will succeed in life and this bootcamp too!", "Happiness in life and work awaits you!", "Don't focus on the hard times, focus on your code!"];
+
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+    },
+
+    getMotivated: (req, res) => {
+        const motivations = ["You've got this!", "If you're reading this, consider yourself blessed!", "Success is inevitable!", "Just do it!", "Put in the work and the results will follow"];
+
+        let randomIndex = Math.floor(Math.random() * motivations.length);
+        let randomMotivations= motivations[randomIndex];
+
+        res.status(200).send(randomMotivations);
+    },
+
+    // getClicked: (req, res) => {
+    // const yesReponse = "Your health thanks you!!"
+    // res.status(200).send(yesReponse);
+    // }
+
+}
